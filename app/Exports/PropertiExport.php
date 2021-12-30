29a83718d73b7860e824f7a1ba6246064e3a0a3e
erlanggadw <erlangga.dewarangga@gmail.com>
2021-12-30 17:06:05 +0700

    export excel

--- app/Exports/PropertiExport.php
+++ app/Exports/PropertiExport.php
@@ -13,7 +13,18 @@ class PropertiExport implements FromCollection
     */
     public function collection()
     {
-        return properti::all();
+        return properti::get([
+            'id', 'user_id', 'category_id', 'jenis', 'nama', 'deskripsi',
+            'alamat_gmap', 'provinsi', 'kabupaten', 'kecamatan', 'kode_pos',
+            'luas_tanah', 'luas_bangunan', 'sertifikat', 'umur_bangunan',
+            'jumlah_lantai', 'kamar_tidur', 'kamar_mandi', 'kamar_tidur_art',
+            'kamar_mandi_art', 'daya_listrik', 'orientasi_bangunan', 'tahun_dibangun',
+            'interior', 'fasilitas', 'pdam', 'foto_tampak_depan', 'foto_tampak_jalan',
+            'foto_tampak_ruangan', 'harga', 'cicilan', 'uang_muka', 'nego',
+            'harga_uang_muka', 'longitude', 'latitude', 'whatsapp', 'kontak',
+            'tayang', 'pet_allowed', 'created_at', 'updated_at', 'deleted_at',
+            'exp'
+        ]);
     }
 
     public function headings(): array {
@@ -24,7 +35,7 @@ class PropertiExport implements FromCollection
             'Kamar Tidur', 'Kamar Mandi', 'Kamar Tidur ART', 'Kamar Mandi ART', 'Daya Listrik',
             'Orientasi Bangunan', 'Tahun Dibangun', 'Interior', 'Fasilitas',
             'PDAM', 'URL Foto Tampak Depan', 'URL Foto Tampak Jalan',
-            'URL Foto Tampak Ruangan', 'URL Foto Tampak Lain', 'Harga', 'Cicilan', 
+            'URL Foto Tampak Ruangan', 'Harga', 'Cicilan', 
             'Uang Muka', 'Nego', 'Harga Uang Muka', 'Longitude', 'Latitude',
             'WhatsApp', 'Kontak', 'Tayang', 'Pet Allowed', 'Dibuat Pada', 'Diupdate Pada',
             'Kadaluwarsa Pada'

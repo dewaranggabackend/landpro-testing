612ace094589b7d598b042f14f14c9086ba30d1e
erlanggadw <erlangga.dewarangga@gmail.com>
2021-12-31 15:21:49 +0700

    aa

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -50,7 +50,7 @@ Dashboard
                             </div>
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-danger text-white mb-4">
-                                    <div class="card-body"> <i class="fa fa-house-user"></i> &nbsp;{{$off_properti}} Properti Nonaktif / Trashed</div>
+                                    <div class="card-body"> <i class="fa fa-house-user"></i> &nbsp;{{$off_properti}} Properti Nonaktif / Dihapus</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="/properti/nonaktif">Lihat</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>

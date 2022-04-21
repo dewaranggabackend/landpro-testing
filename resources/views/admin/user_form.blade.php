8c70e8cc8aa90e8ecb5a0a345d5b3eebdb1421e4
erlanggadw <erlangga.dewarangga@gmail.com>
2022-04-21 09:41:44 +0700

    #5 Testing fitur add customer service: add back button cust service form

--- resources/views/admin/user_form.blade.php
+++ resources/views/admin/user_form.blade.php
@@ -13,4 +13,9 @@
 @endsection
 
 @section('body')
+    <div class="card mb-4">
+        <div class="card-body">
+            <a href="/users"><button type="button" class="btn btn-secondary" data-toggle="tooltip" title="Kembali ke List User"><i class="fa fa-arrow-left"></i></button></a>
+        </div>
+    </div>
 @endsection

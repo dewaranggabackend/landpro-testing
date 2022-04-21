eb7b4b7c110daf1d586671d19a5f5e648b4dc68d
erlanggadw <erlangga.dewarangga@gmail.com>
2022-04-21 09:45:34 +0700

    #6 Testing fitur add customer service: test field nama

--- resources/views/admin/user_form.blade.php
+++ resources/views/admin/user_form.blade.php
@@ -18,4 +18,15 @@
             <a href="/users"><button type="button" class="btn btn-secondary" data-toggle="tooltip" title="Kembali ke List User"><i class="fa fa-arrow-left"></i></button></a>
         </div>
     </div>
+
+    <div class="card mb-4">
+        <div class="card-body">
+            <form action="/users/customer-service/create" method="POST">
+                <div id="Nama">
+                    <label for="nama">Nama</label>
+                    <input id="nama" type="text" required>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
